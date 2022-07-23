e7223b7d3bd32d170213cfc4044b0894bf79e2a9
Brian Wo <45139213+brainwo@users.noreply.github.com>
2022-07-24 07:36:00 +0800

    feat: add set_camera

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,13 @@ async fn main() {
         //  draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
         //  draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
         //  draw_text("HELLO", 20.0, 20.0, 20.0, DARKGRAY);
+        //
+        set_camera(&Camera2D {
+            zoom: vec2(1., screen_width() / screen_height()),
+            ..Default::default()
+        });
+
+        set_default_camera();
 
         next_frame().await
     }
