6d9ea75ccf5c164c680828d313ae8d2ec06bd19d
Brian Wo <45139213+brainwo@users.noreply.github.com>
2022-07-24 07:39:33 +0800

    feat: add draw_checkerboard()

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,25 @@ async fn main() {
 
         set_default_camera();
 
+        draw_checkerboard();
+
         next_frame().await
+    }
+}
+
+pub fn draw_checkerboard() {
+    for i in 0..=(screen_width() / 20.) as u32 {
+        for j in 0..=(screen_height() / 20.) as u32 {
+            draw_rectangle(
+                i as f32 * 20. - 10.,
+                j as f32 * 20. - 10.,
+                20.,
+                20.,
+                match (i + j) % 2 {
+                    0 => Color::from_rgba(43, 46, 51, 255),
+                    _ => Color::from_rgba(59, 62, 67, 255),
+                },
+            )
+        }
     }
 }
